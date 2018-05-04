abf7c1e72d8b47d9b69df6969a59e2c559ac2af3
hery <2940045321@qq.com>
2018-05-04 10:19:42 +0800

    5-4   10:19   数据类型和变量  字符串

---
+++ 1.py
@@ -0,0 +1,13 @@
+#print('hello, world','asdasd','asdasd');
+# print()会依次打印每个字符串，遇到逗号“,”会输出一个空格，因此，输出的字符串是这样拼起来的
+
+ # name = input();
+
+
+# name = input('plz input  ')
+
+# print('hello,', name)
+
+# print ('siisisi',1024 * 768 )
+
+print(1.2e-5)
